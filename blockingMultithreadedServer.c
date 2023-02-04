7901470a1974b41c67246038397d9b2da32ea597
JP Palacios <56488109+JPPalacios@users.noreply.github.com>
2023-02-03 21:23:14 -0500

    Create blockingMultithreadedServer.c

---
+++ blockingMultithreadedServer.c
@@ -0,0 +1,96 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+
+#define FILENAME_SIZE 25
+
+void sigHandler(int sigNum);
+void getFileName(void);
+void *dispatchFile(void *arg);
+void *workerFileAccess(void *arg);
+
+char filename[FILENAME_SIZE];
+int fileRequests = 0;
+
+int main(void)
+{
+    pthread_t dispatchThread;
+    void *dispatchThreadResult;
+    int dispatchThreadStatus;
+    int joinStatus;
+
+    srand(time(NULL));
+    signal(SIGINT, sigHandler);
+
+    while(1)
+    {
+        dispatchThreadStatus = pthread_create(&dispatchThread, NULL, dispatchFile, NULL);
+        // sleep(1);
+
+        joinStatus = pthread_join(dispatchThread, &dispatchThreadResult);
+
+        if (joinStatus != 0)
+        {
+            fprintf(stderr, "[main thread] - Join error %d: %s\n", joinStatus, strerror(joinStatus));
+            exit(1);
+        }
+    }
+
+    return 0;
+}
+
+void *workerFileAccess(void *arg)
+{
+    int nMin = 7, nMax = 10;
+    fileRequests++;
+    // simulates file access duration
+    (rand() % 10 < 8) ? sleep(1) : sleep(rand() % ((nMax + 1) - (nMin) + nMin) );
+
+    printf("\nworker received filename: \"%s\"\n", (char *) arg);
+
+    return NULL;
+}
+
+void *dispatchFile(void *arg)
+{
+    getFileName(); // prompt user for a filename
+
+    pthread_t workerThread;
+    void *workerThreadResult;
+    int workerThreadStatus;
+    int joinStatus;
+
+    workerThreadStatus = pthread_create(&workerThread, NULL, workerFileAccess, filename);
+
+    joinStatus = pthread_join(workerThread, &workerThreadResult);
+
+    if (joinStatus != 0)
+    {
+        fprintf(stderr, "[dispatch thread] - Join error %d: %s\n", joinStatus, strerror(joinStatus));
+        exit(1);
+    }
+
+    return workerThreadResult;
+}
+
+void getFileName(void)
+{
+    fputs("\nEnter a filename: ", stdout);
+    fgets(filename, sizeof(filename), stdin);
+    filename[strlen(filename) - 1] = '\0';
+}
+
+void sigHandler(int sigNum)
+{
+    printf(" received. Displaying program statistics...\n");
+    printf("\n---------- Program Statistics ---------- \n");
+    printf("File requests: %d\n", fileRequests);
+    printf("---------------------------------------- \n");
+    printf("Terminating program...\n");
+    exit(0);
+}
