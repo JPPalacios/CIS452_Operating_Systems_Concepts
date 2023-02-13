207e30cbe26bdd4a90e0af3e859f89c04a79f326
JP Palacios <56488109+JPPalacios@users.noreply.github.com>
2023-02-13 18:25:12 -0500

    Update read.c

--- read.c
+++ read.c
@@ -11,6 +11,11 @@
 
 #include "common.h"
 
+void keyboard_interrupt(int signalNumber);
+void call_status(int functionCall, char *message);
+
+messageType *sharedMemoryPointer;
+
 int main(void)
 {
     // handling keyboard-interruption ctrl-c
@@ -25,14 +30,16 @@ int main(void)
     call_status(sharedMemoryID = shmget(key, sizeof(messageType), SHARED_MEM_OPTIONS), "writer: unable to get shared memory\n");
 
     // attach this file to shared memory segment
-    messageType *sharedMemoryPointer;
     call_status(((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void *) -1), "writer: Unable to attach\n");
 
-    while(1)
+    // cleared cleanup flag allows program to continue
+    (*sharedMemoryPointer).cleanup = 0;
+
+    while(!(*sharedMemoryPointer).cleanup)
     {
         if((*sharedMemoryPointer).turn == READER_TURN)
         {
-            printf("[reader] - message: %s\n", (*sharedMemoryPointer).message);
+            printf("\n [reader] message: %s\n", (*sharedMemoryPointer).message);
             (*sharedMemoryPointer).turn = WRITER_TURN;
         }
     }
@@ -44,4 +51,35 @@ int main(void)
     call_status(shmctl(sharedMemoryID, IPC_RMID, 0), "writer: unable to deallocate\n");
 
     return 0;
+}
+
+void keyboard_interrupt(int signalNumber)
+{
+    printf(" received. Terminating program...\n");
+
+    // cleanup flag set HIGH allows writer to clean up
+    (*sharedMemoryPointer).cleanup = 1;
+
+    // detach from shared memory segment
+    call_status(shmdt(sharedMemoryPointer), "reader: unable to detach\n");
+
+    exit(EXIT_SUCCESS);
+}
+
+void call_status(int functionCall, char *message)
+{
+    if (functionCall < 0)
+    {
+        if (strcmp(message, " ") == 0)
+        {
+            // cleanup flag set HIGH allows reader to clean up
+            (*sharedMemoryPointer).cleanup = 1;
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror(message);
+            exit(EXIT_FAILURE);
+        }
+    }
 }
