afacb8ace7f25b9f30556734d7f8c1bf5cad6eb0
JP Palacios <56488109+JPPalacios@users.noreply.github.com>
2023-02-13 09:46:13 -0500

    Update common.h

--- common.h
+++ common.h
@@ -15,13 +15,21 @@
 #include <sys/shm.h>
 
 #define MEM_BYTE_SIZE 4096
-
+#define WRITER_TURN 1
+#define READER_TURN 2
 #define SHARED_MEM_OPTIONS (IPC_CREAT | S_IRUSR | S_IWUSR)
+
+int value;
+int turn;
+
+void set_turn(int turn_value);
+
+int get_turn(void);
 
 void get_user_input(char *message);
 
-void signal_handler(void);
+void keyboard_interrupt(int signal_number);
 
-void call_handler(int function_call, char *message);
+void call_status(int function_call, char *message);
 
 #endif // COMMON_H_
