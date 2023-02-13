048d4c78c4f28e919fe149be185d120ecc3619b6
JP Palacios <56488109+JPPalacios@users.noreply.github.com>
2023-02-13 09:45:24 -0500

    Update read.c

--- read.c
+++ read.c
@@ -18,25 +18,32 @@ int main(void)
     char *sharedMemoryPointer;
 
     // handling keyboard-interruption ctrl-c
-    signal(SIGINT, signal_handler);
+    signal(SIGINT, keyboard_interrupt);
     printf("Running reader program...\n");
 
     // generate a unique key
     key_t key;
+    call_status(key = ftok("./src/read.c", 65), "Unique key failed\n");
 
-    call_handler(key = ftok("./src/read.c", 65), "Unique key failed\n");
+    call_status(sharedMemoryID = shmget(key, MEM_BYTE_SIZE, SHARED_MEM_OPTIONS), "reader: unable to get shared memory\n");
 
-    call_handler(sharedMemoryID = shmget(key, MEM_BYTE_SIZE, SHARED_MEM_OPTIONS), "reader: unable to get shared memory\n");
+    call_status(((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void *) -1), "reader: unable to attach\n");
 
-    call_handler(((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void *) -1), "reader: unable to attach\n");
+    int *turn = (int *) sharedMemoryPointer;
 
-    // printf("shared contents: %s\n", sharedMemoryPointer);
-    while(strcmp(sharedMemoryPointer, "") == 0);
-    printf("[reader] - message: %s\n", sharedMemoryPointer);
+    while(1)
+    {
+        if(*turn == READER_TURN)
+        {
+            printf("[reader] - message: %s\n", sharedMemoryPointer + sizeof(int));
+            *turn = WRITER_TURN;
+        }
+    }
 
-    call_handler(shmdt(sharedMemoryPointer), "reader: unable to detach\n");
+    // functions should be called when shutting down the program
+    call_status(shmdt(sharedMemoryPointer), "reader: unable to detach\n");
 
-    call_handler(shmctl(sharedMemoryID, IPC_RMID, 0), "reader: unable to deallocate\n");
+    call_status(shmctl(sharedMemoryID, IPC_RMID, 0), "reader: unable to deallocate\n");
 
     return 0;
 }
