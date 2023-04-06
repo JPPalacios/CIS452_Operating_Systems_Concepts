ff669e7f1bf8b940c12fcfd31cdd6c5f51bc6657
Juan Paulo Palacios-Vilchis <palacioj@eos20.cis.gvsu.edu>
2023-04-06 01:01:09 -0400

    jesse we need to cook

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,26 +13,68 @@
 
 #include "main.h"
 
-// need to implement semaphores, threads, and shared memory
-sem_t semaphore;
-
-#define SIZE 32
-
 int main(void)
 {
-
     logs("launching program...");
-    sem_init(&semaphore, 0, 1);
 
     signal(SIGINT, keyboard_interrupt);
 
+    control_semaphores(SETUP);
+
     create_bakers();
 
-    // launch_program()
-
-    sem_destroy(&semaphore);
+    control_semaphores(DESTROY);
 
     return 0;
+}
+
+void control_semaphores(int setup)
+{
+    if (setup == 1)
+    {
+        logs("setting up mutex and semaphores");
+
+        pthread_mutex_init(&mutex, NULL);
+
+        for (int i = 0; i < MIXER_COUNT; i++)
+            sem_init(&mixer[i], 0, 1);
+        
+        sem_init(&pantry, 0, 1);
+
+        for (int i = 0; i < FRIDGE_COUNT; i++)
+            sem_init(&fridge[i], 0, 1);
+
+        for (int i = 0; i < BOWL_COUNT; i++)
+            sem_init(&bowl[i], 0, 1);
+
+        for (int i = 0; i < SPOON_COUNT; i++)
+            sem_init(&spoon[i], 0, 1);
+
+        sem_init(&oven, 0, 1);
+    }
+    else
+    {
+        logs("destroying mutex and semaphores");
+
+        pthread_mutex_destroy(&mutex);
+
+          for (int i = 0; i < MIXER_COUNT; i++)
+            sem_destroy(&mixer[i]);
+
+        sem_destroy(&pantry);
+
+        for (int i = 0; i < FRIDGE_COUNT; i++)
+            sem_destroy(&fridge[i]);
+
+        for (int i = 0; i < BOWL_COUNT; i++)
+            sem_destroy(&bowl[i]);
+
+        for (int i = 0; i < SPOON_COUNT; i++)
+            sem_destroy(&spoon[i]);
+
+        sem_destroy(&oven);
+    }
+
 }
 
 void create_bakers(void)
@@ -39,11 +82,14 @@ void create_bakers(void)
     int status;
     int nBakers = get_baker_value();
     pthread_t baker_thread[nBakers];
+    int temp = 0;
 
-    logs("creating our bakers...");
+    logs("bakers getting ready...");
+
     for (int i = 0; i < nBakers; i++)
     {
-        status = pthread_create(&baker_thread[i], NULL, run_baker_thread, (void *) &i);
+        temp = i;
+        status = pthread_create(&baker_thread[i], NULL, run_baker_thread, (void *) &temp);
         check_status("pthread_create() error", status);
     }
 
@@ -53,32 +99,123 @@ void create_bakers(void)
         check_status("pthread_join() error", status);
     }
 
-    logs("threads created");
+    logs("all bakers finished!");
+}
+
+void get_pantry_ingredients(int recipe, int baker)
+{
+    printf("baker %d waiting to get pantry ingredients...\n", baker);
+
+    sem_wait(&pantry);
+
+    switch (recipe) {
+        case 0:
+            printf("baker %d got flour and sugar from the pantry\n", baker);
+            break;
+        case 1:
+            printf("baker %d got flour, sugar, baking soda, and salt from the pantry\n", baker);
+            break;
+        case 2:
+            printf("baker %d got yeast, sugar, and salt from the pantry\n", baker);
+            break;
+        case 3:
+            printf("baker %d got flour, sugar, salt, yeast, and baking soda from the pantry\n", baker);
+            break;
+        case 4:
+            printf("baker %d got flour, sugar, salt, and cinnamon from the pantry\n", baker);
+            break;
+        default:
+            printf("baker %d did not get any ingredients from the pantry\n", baker);
+    }
+
+    sem_post(&pantry);
+}
+
+void get_fridge_ingredients(int recipe, int baker)
+{
+
+    printf("baker %d waiting to get fridge ingredients...\n", baker);
+
+    // todo: find a way to implement utensils
+    // for(int i = 0; i < FRIDGE_COUNT; i++)
+    // {
+    //     pthread_mutex_lock(&mutex);
+
+    //     sem_wait(&fridge[i]);
+    //     printf("baker %d in fridge %d.\n\n", baker, i);
+    //     get_fridge_ingredients();
+    //     sem_post(&fridge[i]);
+        
+    //     pthread_mutex_unlock(&mutex);
+    // }
+    
+    switch (recipe) {
+        case 0:
+            printf("baker %d got milk and butter from the fridge\n", baker);
+            break;
+        case 1:
+            printf("baker %d got egg, milk, and butter from the fridge\n", baker);
+            break;
+        case 2:
+            printf("baker %d did not get any ingredients from the fridge\n", baker);
+            break;
+        case 3:
+            printf("baker %d got eggs from the fridge\n", baker);
+            break;
+        case 4:
+            printf("baker %d got butter and eggs from the fridge\n", baker);
+            break;
+        default:
+            printf("baker %d did not get any ingredients from the fridge\n", baker);
+    }
+}
+
+void get_utensils(int baker)
+{
+
 }
 
 void *run_baker_thread(void *arg)
 {
+    int baker = *((int *) arg);
 
-    /* baker getting ingredients */
-    printf("baker %d waiting to get ingredients...\n", *((int *) arg));
-    sem_wait(&semaphore);
-    // get_ingredients();
-    sem_post(&semaphore);
-    printf("baker %d got ingredients!\n", *((int *) arg));
+    int current_recipe = 0;
 
-    /* baker getting cooking utensils */
-    printf("baker %d waiting to get utensils...\n", *((int *) arg));
-    sem_wait(&semaphore);
-    // get_utensils();
-    sem_post(&semaphore);
-    printf("baker %d got utensils!\n", *((int *) arg));
+    // todo: fix semaphore baker value
+    // todo: implement utensil sems
+    // 
 
-    /* baker cooking recepe in oven */
-    printf("baker %d waiting to use the oven...\n", *((int *) arg));
-    sem_wait(&semaphore);
-    // use_oven();
-    sem_post(&semaphore);
-    printf("baker %d finished recepe!\n", *((int *) arg));
+    while(current_recipe < TOTAL_RECIPE_COUNT)
+    {
+        /* baker getting ingredients from pantry */
+        get_pantry_ingredients(current_recipe, baker);
+
+        /* baker getting ingredients from fridge */
+        get_fridge_ingredients(current_recipe, baker);
+
+        // /* baker getting cooking utensils */
+        // printf("baker %d waiting to get utensils...\n", baker);
+        // // get_utensils(baker);
+        // sem_wait(&bowl[0]);
+        // sem_wait(&mixer[0]);
+        // sem_wait(&spoon[0]);
+
+        // sem_post(&spoon[0]);
+        // sem_post(&mixer[0]);
+        // sem_post(&bowl[0]);
+        // printf("baker %d got utensils.\n\n", baker);
+
+        /* baker cooking recipe in oven */
+        printf("baker %d waiting to use the oven...\n", baker);
+        sem_wait(&oven);
+        printf("baker %d using oven.\n\n", baker);
+        sem_post(&oven);
+
+        printf("\nbaker %d finished the %s recipe!\n\n", baker, recipe_book[current_recipe]);
+        current_recipe++;
+    }
+
+
 
     pthread_exit(NULL);
 }
@@ -92,7 +229,7 @@ void keyboard_interrupt(int signalNumber)
         logs("terminating program...");
 
         /* removes semaphore */
-        sem_destroy(&semaphore);
+        control_semaphores(DESTROY);
 
         exit(EXIT_SUCCESS);
     }
@@ -106,7 +243,7 @@ int get_baker_value(void)
 
     do
     {
-        printf(">$ Enter the number of bakers: ");
+        printf("\n>$ Enter the number of bakers: ");
         fgets(buffer, sizeof(buffer), stdin);
 
         user_error = 0;
--- src/main.h
+++ src/main.h
@@ -3,9 +3,38 @@
 
 #define BUFFER_SIZE 32
 
+#define MIXER_COUNT  2
+#define FRIDGE_COUNT 2
+#define BOWL_COUNT   3
+#define SPOON_COUNT  5
+
+#define TOTAL_RECIPE_COUNT 5
+
+#define SETUP 1
+#define DESTROY 0
+
+sem_t mixer[MIXER_COUNT];
+sem_t pantry;
+sem_t fridge[FRIDGE_COUNT];
+sem_t bowl[BOWL_COUNT];
+sem_t spoon[SPOON_COUNT];
+sem_t oven;
+
+pthread_mutex_t mutex;
+
+char *recipe_book[TOTAL_RECIPE_COUNT] = {"cookies", "pankcakes", "pizza dough", "pretzels", "cinnamon rolls"};
+
 /* Program */
 
+void control_semaphores(int setup);
+
 void create_bakers(void);
+
+void get_pantry_ingredients(int recipe, int baker);
+
+void get_fridge_ingredients(int recipe, int baker);
+
+void get_utensils(int baker);
 
 void *run_baker_thread(void *arg);
 
